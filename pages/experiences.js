0b55c1c77f357fc216891405fabbb3d2c989cd33
luchofazio27 <luchofazio@gmail.com>
2023-08-16 23:14:24 -0300

    creacion de pagina experiences.js

--- pages/experiences.js
+++ pages/experiences.js
@@ -1,4 +1,5 @@
 import Layout from "@/components/layout";
+import styles from "../styles/experiences.module.css"
 
 export default function Experiences() {
   return (
@@ -6,7 +7,46 @@ export default function Experiences() {
       title={"Experiencias laborales"}
       description={"Experiencias laborales, camino recorrido en IT"}
     >
-      <h1>Experiencias</h1>
+      <main className="container">
+        <h1 className="heading">Experiencias</h1>
+
+        <div>
+          <div>
+            <p>
+              <b className={styles.work}>PwC</b><br></br>
+              <b className={styles.category}>Specialist Technical Support</b> // <b>dic 2021 - presente.</b><br></br>Soporte al
+              usuario interno de forma presencial y remota. Responsable de las
+              garantías y coordinación con las marcas de laptops. Manejo de
+              ticketera Service Now.
+            </p>
+
+            <p>
+            <b className={styles.work}>Compumundo / Garbarino</b><br></br>
+              <b className={styles.category}>Técnico informático</b> // <b>abr 2013 - dic 2021</b><br></br>
+              Asistencia y resolución de problemas internos en sucursales de la
+              empresa (Garbarino, Compumundo, Samsung Store, Ipoint, Garbarino
+              Viajes) a nivel software, hardware e infraestructura de redes y
+              conexión. Armado e instalación de puestos de trabajo (PC,
+              periféricos, puesto de red, controlador fiscal, impresora y
+              cuponera). Representante y reparación directa de las marcas
+              Samsung (Notebooks, tablets y celulares) y Asus (Notebook).
+              Técnico en servicio de asistencia al domicilio de cliente
+              (reparación a niveles software y asesoramiento sobre diversos
+              temas de tecnología).
+            </p>
+
+            <p>
+            <b className={styles.work}>Depilight</b><br></br>
+            <b className={styles.category}>Técnico informático</b> // <b>mar 2011 - abr 2013</b><br></br>Soporte técnico
+              para usuario interno (Software y Hardware) en el call center
+              central. Administración de sistema interno. Mantenimiento e
+              infraestructura tanto en call center como en sucursales. Soporte
+              de forma remota y presencial a dichas sucursales sobre consultas o
+              urgencias informáticas.
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 }
--- pages/studies.js
+++ pages/studies.js
@@ -1,4 +1,7 @@
+import Image from "next/image";
 import Layout from "@/components/layout";
+import styles from "../styles/studies.module.css";
+import Link from "next/link";
 
 export default function Studies() {
   return (
@@ -6,7 +9,37 @@ export default function Studies() {
       title={"Estudios"}
       description={"Certificaciones y estudios en curso"}
     >
-      <h1>Estudios</h1>
+      <main className="container">
+        <h1 className="heading">Estudios</h1>
+
+        <div className={styles.content}>
+          <Image
+            src="/img/diplomaDigitalHouse.jpg"
+            width={1000}
+            height={800}
+            alt="Diploma en Digital House"
+          />
+
+          <div>
+            <p><b>Programación Web Full Stack</b><br></br><Link href="https://www.digitalhouse.com/ar" target="_black">Digital House</Link><br></br>2022 - 2023</p>
+
+            
+            <p>
+              <b>Técnico informatico</b> <br></br>IAC<br></br>2009 -
+              2010
+            </p>
+
+            <p>
+              <b>Bachillerato</b> <br></br>Escuela de educación media n° 6 José Gervasio
+              Artigas <br></br>2007
+            </p>
+
+            <p>
+            <b>React - La Guía Completa</b> <br></br><Link href="https://www.udemy.com/" target="_black">Udemy</Link><br></br>Cursando
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 }
