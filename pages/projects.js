49740e9fb230a87c0353a067fb9e0334e8b05c2f
luchofazio27 <luchofazio@gmail.com>
2023-12-27 16:17:31 -0300

    modificacion en projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,23 +1,23 @@
-import Layout from "@/components/layout"
-import Project from "@/components/project";
+// import Layout from "@/components/layout"
+// import Project from "@/components/project";
 
-export default function Projects({projects}) {
-  return (
-    <Layout title={'Mis Proyectos'} description="Proyectos trabajados">
-      {projects.map(project => (
-        <Project key={project.id} project={project.attributes}/>
-      ))}
-    </Layout>
-  )
-}
+// export default function Projects({projects}) {
+//   return (
+//     <Layout title={'Mis Proyectos'} description="Proyectos trabajados">
+//       {projects.map(project => (
+//         <Project key={project.id} project={project.attributes}/>
+//       ))}
+//     </Layout>
+//   )
+// }
 
-export async function getStaticProps() {
-    const answer = await fetch(`${process.env.API_URL}/projects?populate=*`);
-    const { data: projects } = await answer.json();
+// export async function getStaticProps() {
+//     const answer = await fetch(`${process.env.API_URL}/projects?populate=*`);
+//     const { data: projects } = await answer.json();
     
-    return {
-      props: {
-        projects
-      },
-    };
-  }
+//     return {
+//       props: {
+//         projects
+//       },
+//     };
+//   }
